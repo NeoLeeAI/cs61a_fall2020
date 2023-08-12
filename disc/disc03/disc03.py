6cbac6e2d36ede4485fb34bf3107bb8e197a4e12
xuhaoran <NeoLeeAI@outlook.com>
2023-08-12 12:55:51 +0800

    2023.8.12

--- disc/disc03/disc03.py
+++ disc/disc03/disc03.py
@@ -32,3 +32,56 @@ def hailstone(n):
     else:
         return 1 + hailstone(n * 3 + 1)
     
+# 1.4
+def merge(n1, n2):
+    """ Merges two numbers
+    >>> merge(31, 42)
+    4321
+    >>> merge(21, 0)
+    21
+    >>> merge (21, 31)
+    3211
+    """
+    if n1 == 0:
+        return n2
+    elif n2 == 0:
+        return n1
+    elif n1 % 10 < n2 % 10:
+        return n1 % 10 + merge(n1 // 10, n2) * 10
+    else:
+        return n2 % 10 + merge(n1, n2 // 10) * 10
+
+# 1.5
+def make_func_repeater(f, x):
+    """
+    >>> incr_1 = make_func_repeater(lambda x: x + 1, 1)
+    >>> incr_1(2) #same as f(f(x))
+    3
+    >>> incr_1(5)
+    6
+    """
+    def repeat(y):
+        if x == 1:
+            return f(y)
+        else:
+            return repeat(x-1)
+    return repeat
+
+# 1.6
+def is_prime(n):
+    """
+    >>> is_prime(7)
+    True
+    >>> is_prime(10)
+    False
+    >>> is_prime(1)
+    False
+    """
+    def prime_helper(i):
+        if i == n:
+            return True
+        elif n % i == 0 or n == 1:
+            return False
+        else:
+            return prime_helper(i + 1)
+    return prime_helper(2)
