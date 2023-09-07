d9866369c05d3d9a0c3e76e4192dbd9c5148ec57
xuhaoran <NeoLeeAI@outlook.com>
2023-09-07 18:42:49 +0800

    done in 2023.9.7

--- disc/disc06/disc06.py
+++ disc/disc06/disc06.py
@@ -85,12 +85,23 @@ def filter(iterable, fn):
 def merge(a, b):
     """
     >>> def sequence(start, step):
-    ... while True:
-    ... yield start
-    ... start += step
+    ...     while True:
+    ...         yield start
+    ...         start += step
     >>> a = sequence(2, 3) # 2, 5, 8, 11, 14, ...
     >>> b = sequence(3, 2) # 3, 5, 7, 9, 11, 13, 15, ...
     >>> result = merge(a, b) # 2, 3, 5, 7, 8, 9, 11, 13, 14, 15
     >>> [next(result) for _ in range(10)]
     [2, 3, 5, 7, 8, 9, 11, 13, 14, 15]
     """
+    a_elm, b_elm = next(a), next(b)
+    while True:
+        if a_elm < b_elm:
+            yield a_elm
+            a_elm = next(a)
+        elif b_elm < a_elm:
+            yield b_elm
+            b_elm = next(b)
+        else:
+            yield a_elm
+            a_elm, b_elm = next(a), next(b)
