6902fb3c84a3280cf0a9bf5b58b5322421036593
xuhaoran <NeoLeeAI@outlook.com>
2023-09-21 11:09:05 +0800

    done in 2023.9.21

--- disc/disc07/disc07.py
+++ disc/disc07/disc07.py
@@ -83,3 +83,63 @@ class Client:
         client.
         """
         self.inbox.append(email)
+#2.1
+class Pet():
+    def __init__(self, name, owner):
+        self.is_alive = True # It's alive!!!
+        self.name = name
+        self.owner = owner
+    def eat(self, thing):
+        print(self.name + " ate a " + str(thing) + "!")
+    def talk(self):
+        print(self.name)
+
+class Dog(Pet):
+    def talk(self):
+        print(self.name + " says woof!")
+
+class Cat(Pet):
+    def __init__(self, name, owner, lives=9):
+        self.is_alive = True
+        self.name = name
+        self.owner = owner
+        self.lives = lives
+    def talk(self):
+        """ Print out a cat's greeting.
+        >>> Cat('Thomas', 'Tammy').talk()
+        Thomas says meow!
+        """
+        print(self.name + ' says meow!')
+    def lose_life(self):
+        """Decrements a cat's life by 1. When lives reaches zero, 'is_alive'
+        becomes False. If this is called after lives has reached zero, print out
+        that the cat has no more lives to lose.
+        """
+        if self.is_alive:
+            self.lives -= 1
+            if self.lives == 0:
+                self.is_alive = False
+        else:
+            print('The cat has no more lives to lose.')
+
+#2.2
+class NoisyCat(Cat): # Fill me in!
+    """A Cat that repeats things twice."""
+    def talk(self):
+        """Talks twice as much as a regular cat.
+        >>> NoisyCat('Magic', 'James').talk()
+        Magic says meow!
+        Magic says meow!
+        """
+        print('{} says meow!'.format(self.name))
+        print('{} says meow!'.format(self.name))
+
+    def __repr__(self):
+        """The interpreter-readable representation of a NoisyCat
+        >>> muffin = NoisyCat('Muffin', 'Catherine')
+        >>> repr(muffin)
+        "NoisyCat('Muffin', 'Catherine')"
+        >>> muffin
+        NoisyCat('Muffin', 'Catherine')
+        """
+        return 'NoisyCat(\'{}\', \'{}\')'.format(str(self.name), str(self.owner))
