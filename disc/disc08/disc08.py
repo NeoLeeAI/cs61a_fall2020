9268f8cb59f33b3e7890c91f8025f67418c47022
xuhaoran <NeoLeeAI@outlook.com>
2023-10-08 16:59:00 +0800

    done q2

---
+++ disc/disc08/disc08.py
@@ -0,0 +1,101 @@
+class Link:
+    empty = ()
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest:
+            rest_str = ', ' + repr(self.rest)
+        else:
+            rest_str = ''
+        return 'Link({0}{1})'.format(repr(self.first), rest_str)
+    
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+#2.1
+def sum_nums(lnk):
+    """
+    >>> a = Link(1, Link(6, Link(7)))
+    >>> sum_nums(a)
+    14
+    """
+    if lnk is Link.empty:
+        return 0
+    else:
+        return lnk.first + sum_nums(lnk.rest)
+#2.2
+def multiply_lnks(lst_of_lnks):
+    """
+    >>> a = Link(2, Link(3, Link(5)))
+    >>> b = Link(6, Link(4, Link(2)))
+    >>> c = Link(4, Link(1, Link(0, Link(2))))
+    >>> p = multiply_lnks([a, b, c])
+    >>> p.first
+    48
+    >>> p.rest.first
+    12
+    >>> p.rest.rest.rest is Link.empty
+    True
+    """
+    # Note: you might not need all lines in this skeleton code
+    product = 1
+    for lnk in lst_of_lnks:
+        if lnk is Link.empty:
+            return Link.empty
+        else:
+            product *= lnk.first
+    return Link(product, multiply_lnks([lnk.rest for lnk in lst_of_lnks]))
+
+#2.3
+def flip_two(lnk):
+    """
+    >>> one_lnk = Link(1)
+    >>> flip_two(one_lnk)
+    >>> one_lnk
+    Link(1)
+    >>> lnk = Link(1, Link(2, Link(3, Link(4, Link(5)))))
+    >>> flip_two(lnk)
+    >>> lnk
+    Link(2, Link(1, Link(4, Link(3, Link(5)))))
+    """
+    if lnk.rest is Link.empty:
+        pass
+    else:
+        temp = lnk.first
+        lnk.first = lnk.rest.first
+        lnk.rest.first = temp
+        if lnk.rest.rest:
+            flip_two(lnk.rest.rest)
+
+#2.4
+def filter_link(link, f):
+    """
+    >>> link = Link(1, Link(2, Link(3)))
+    >>> g = filter_link(link, lambda x: x % 2 == 0)
+    >>> next(g)
+    2
+    >>> list(filter_link(link, lambda x: x % 2 != 0))
+    [1, 3]
+    """
+    pointer = link
+    while pointer != Link.empty:
+        if f(pointer.first):
+            yield pointer.first
+        pointer = pointer.rest
+
+class Tree:
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = branches
+
+    def is_leaf(self):
+        return not self.branches
+    
