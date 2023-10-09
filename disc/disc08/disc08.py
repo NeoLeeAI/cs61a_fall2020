cdee4041662919103f9201c9c957063a2d4d39df
xuhaoran <NeoLeeAI@outlook.com>
2023-10-09 10:16:14 +0800

    done disc08

--- disc/disc08/disc08.py
+++ disc/disc08/disc08.py
@@ -99,3 +99,94 @@ class Tree:
     def is_leaf(self):
         return not self.branches
     
+    def __repr__(self) -> str:
+        if not self.branches:
+            return 'Tree({0})'.format(self.label)
+        return 'Tree({0}, {1})'.format(self.label, self.branches)
+   
+#3.1
+def make_even(t):
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4), Tree(5)])
+    >>> make_even(t)
+    >>> t.label
+    2
+    >>> t.branches[0].branches[0].label
+    4
+    """
+    assert isinstance(t, Tree)
+    if t.label % 2 != 0:
+        t.label += 1
+    for b in t.branches:
+        make_even(b)
+
+#3.2
+def square_tree(t):
+    """Mutates a Tree t by squaring all its elements.
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4), Tree(5)])
+    >>> square_tree(t)
+    >>> t.label
+    1
+    >>> t.branches[0].branches[0].label
+    9
+    >>> t.branches[1].label
+    16
+    """
+    assert isinstance(t, Tree)
+    t.label *= t.label
+    for b in t.branches:
+        square_tree(b)
+
+#3.3
+def find_paths(t, entry):
+    '''
+    >>> tree_ex = Tree(2, [Tree(7, [Tree(3), Tree(6, [Tree(5), Tree(11)])]), Tree(1, [Tree(5)])])
+    >>> find_paths(tree_ex, 5)
+    [[2, 7, 6, 5], [2, 1, 5]]
+    >>> find_paths(tree_ex, 12)
+    []
+    '''
+    assert isinstance(t, Tree)
+    paths = []
+    if t.label == entry:
+        paths.append([t.label])
+    for b in t.branches:
+        if find_paths(b, entry):
+            paths += [[t.label] + path for path in find_paths(b, entry)]
+    return paths
+
+#3.4
+def combine_tree(t1, t2, combiner):
+    """
+    >>> from operator import mul
+    >>> a = Tree(1, [Tree(2, [Tree(3)])])
+    >>> b = Tree(4, [Tree(5, [Tree(6)])])
+    >>> combined = combine_tree(a, b, mul)
+    >>> combined.label
+    4
+    >>> combined.branches[0].label
+    10
+    """
+    assert isinstance(t1, Tree) and isinstance(t2, Tree)
+    if Tree.is_leaf(t1) and Tree.is_leaf(t2):
+        return Tree(combiner(t1.label, t2.label))
+
+    return Tree(
+        combiner(t1.label, t2.label),
+        [combine_tree(b[0], b[1], combiner) for b in zip(t1.branches, t2.branches)],
+    ) 
+
+#3.5
+def alt_tree_map(t, map_fn):
+    """
+    >>> t = Tree(1, [Tree(2, [Tree(3)]), Tree(4)])
+    >>> negate = lambda x: -x
+    >>> alt_tree_map(t, negate)
+    Tree(-1, [Tree(2, [Tree(-3)]), Tree(4)])
+    """
+    assert isinstance(t, Tree)
+    t.label = map_fn(t.label)
+    for b in t.branches:
+        for b_b in b.branches:
+            alt_tree_map(b_b, map_fn)
+    return t
