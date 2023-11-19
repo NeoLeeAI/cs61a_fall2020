f55d5966fae9b0f75beb0ceb4672c88b9d8c71e8
xuhaoran <NeoLeeAI@outlook.com>
2023-11-19 17:45:54 +0800

    done q15 q16

--- hw/scheme/questions.scm
+++ hw/scheme/questions.scm
@@ -1,87 +1,115 @@
 (define (caar x) (car (car x)))
+
 (define (cadr x) (car (cdr x)))
+
 (define (cdar x) (cdr (car x)))
+
 (define (cddr x) (cdr (cdr x)))
 
 ; Some utility functions that you may find useful to implement
+(define (zip pairs) 'replace-this-line)
 
-(define (zip pairs)
-  'replace-this-line)
-
-
-;; Problem 15
-;; Returns a list of two-element lists
+; ; Problem 15
+; ; Returns a list of two-element lists
 (define (enumerate s)
   ; BEGIN PROBLEM 15
-  'replace-this-line
+  (define (helper s num)
+    (if (null? s)
+        '()
+        (cons (cons num (cons (car s) nil))
+              (helper (cdr s) (+ num 1))
+        )
+    )
   )
-  ; END PROBLEM 15
+  (helper s 0)
+)
 
-;; Problem 16
-
-;; Merge two lists LIST1 and LIST2 according to COMP and return
-;; the merged lists.
+; END PROBLEM 15
+; ; Problem 16
+; ; Merge two lists LIST1 and LIST2 according to COMP and return
+; ; the merged lists.
 (define (merge comp list1 list2)
   ; BEGIN PROBLEM 16
-  'replace-this-line
+  (cond 
+    ((null? list1)
+     list2
+    )
+    ((null? list2)
+     list1
+    )
+    ((comp (car list1) (car list2))
+     (cons (car list1) (merge comp (cdr list1) list2))
+    )
+    (else
+     (cons (car list2) (merge comp list1 (cdr list2)))
+    )
   )
-  ; END PROBLEM 16
+)
 
-
+; END PROBLEM 16
 (merge < '(1 5 7 9) '(4 8 10))
+
 ; expect (1 4 5 7 8 9 10)
 (merge > '(9 7 5 1) '(10 8 4 3))
+
 ; expect (10 9 8 7 5 4 3 1)
-
-;; Problem 17
-
+; ; Problem 17
 (define (nondecreaselist s)
-    ; BEGIN PROBLEM 17
-    'replace-this-line
-    )
-    ; END PROBLEM 17
+  ; BEGIN PROBLEM 17
+  'replace-this-line
+)
 
-;; Problem EC
-;; Returns a function that checks if an expression is the special form FORM
+; END PROBLEM 17
+; ; Problem EC
+; ; Returns a function that checks if an expression is the special form FORM
 (define (check-special form)
-  (lambda (expr) (equal? form (car expr))))
+  (lambda (expr) (equal? form (car expr)))
+)
 
 (define lambda? (check-special 'lambda))
+
 (define define? (check-special 'define))
+
 (define quoted? (check-special 'quote))
-(define let?    (check-special 'let))
 
-;; Converts all let special forms in EXPR into equivalent forms using lambda
+(define let? (check-special 'let))
+
+; ; Converts all let special forms in EXPR into equivalent forms using lambda
 (define (let-to-lambda expr)
-  (cond ((atom? expr)
-         ; BEGIN PROBLEM EC
-         'replace-this-line
-         ; END PROBLEM EC
-         )
-        ((quoted? expr)
-         ; BEGIN PROBLEM EC
-         'replace-this-line
-         ; END PROBLEM EC
-         )
-        ((or (lambda? expr)
-             (define? expr))
-         (let ((form   (car expr))
-               (params (cadr expr))
-               (body   (cddr expr)))
-           ; BEGIN PROBLEM EC
-           'replace-this-line
-           ; END PROBLEM EC
-           ))
-        ((let? expr)
-         (let ((values (cadr expr))
-               (body   (cddr expr)))
-           ; BEGIN PROBLEM EC
-           'replace-this-line
-           ; END PROBLEM EC
-           ))
-        (else
-         ; BEGIN PROBLEM EC
-         'replace-this-line
-         ; END PROBLEM EC
-         )))
-
+  (cond 
+    ((atom? expr)
+     ; BEGIN PROBLEM EC
+     'replace-this-line
+     ; END PROBLEM EC
+    )
+    ((quoted? expr)
+     ; BEGIN PROBLEM EC
+     'replace-this-line
+     ; END PROBLEM EC
+    )
+    ((or (lambda? expr) (define? expr))
+     (let ((form (car expr))
+           (params (cadr expr))
+           (body (cddr expr))
+          )
+       ; BEGIN PROBLEM EC
+       'replace-this-line
+       ; END PROBLEM EC
+     )
+    )
+    ((let? expr)
+     (let ((values (cadr expr))
+           (body (cddr expr))
+          )
+       ; BEGIN PROBLEM EC
+       'replace-this-line
+       ; END PROBLEM EC
+     )
+    )
+    (else
+     ; BEGIN PROBLEM EC
+     'replace-this-line
+     ; END PROBLEM EC
+    )
+  )
+)
