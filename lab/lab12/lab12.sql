6a615bd4856c45c7b465339d346dd0ac801a4849
xuhaoran <NeoLeeAI@outlook.com>
2023-11-27 11:16:06 +0800

    done all

--- lab/lab12/lab12.sql
+++ lab/lab12/lab12.sql
@@ -21,9 +21,13 @@ CREATE TABLE smallest_int AS
 
 
 CREATE TABLE matchmaker AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
-
+  SELECT first.pet as pet, first.song as song, first.color, second.color
+  FROM students as first, students as second
+  WHERE first.time != second.time AND first.pet = second.pet AND first.song = second.song;
+  
 
 CREATE TABLE sevens AS
-  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
+  SELECT a.seven
+  FROM students as a, numbers as b
+  WHERE a.time = b.time AND a.number = 7 AND b.'7' = "True";
 
